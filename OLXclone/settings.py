b4ed5d47c1f3d211d8ed0083b96b1880f7af2f1e
Tanmay Ambadkar <tanmay.ambadkar@gmail.com>
2020-05-27 23:32:01 +0530

    django-summernote

--- OLXclone/settings.py
+++ OLXclone/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 
+SECRET_KEY =
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'UserUI',
     'SellerUI',
-    'BrokerUI'
+    'BrokerUI',
+    'django_summernote',
 ]
 
 MIDDLEWARE = [
